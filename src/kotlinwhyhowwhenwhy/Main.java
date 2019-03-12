90f41c091844fd172cb57987b74392808add6aef
Nino Handler <nih@adorsys.de>
2019-03-12 21:22:57 +0100

    8 - Calling Kotlin from Java

--- src/kotlinwhyhowwhenwhy/Main.java
+++ src/kotlinwhyhowwhenwhy/Main.java
@@ -1,7 +1,36 @@
 package kotlinwhyhowwhenwhy;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+import kotlinwhyhowwhenwhy.classes.CustomerKotlin;
+import kotlinwhyhowwhenwhy.extensions.CustomerExtensionsKt;
+import kotlinwhyhowwhenwhy.higherorder.HigherOrderFunctions;
+
 public class Main {
     public static void main(String[] args) {
+        CustomerKotlin customer = new CustomerKotlin(0, "Fritz", "fritz@gmail.com");
 
+        // TODO make next line compile
+        CustomerKotlin customerOverloaded = new CustomerKotlin(0);
+
+        // TODO make class name more beautiful
+        CustomerExtensionsKt.hasEmail(customer);
+
+        // TODO make next line compile
+        //System.out.println(customer.name);
+
+        // TODO make this more readable
+        HigherOrderFunctions.INSTANCE.triggerWorkInBackground(new Function1<String, Unit>() {
+            @Override
+            public Unit invoke(String result) {
+                System.out.println(result);
+                return null;
+            }
+        });
+
+        // TODO show how to force catch checked exceptions
+
+        // TODO make next line compile
+        //System.out.println(customer.getUniqueName());
     }
 }
--- src/kotlinwhyhowwhenwhy/Main.kt
+++ src/kotlinwhyhowwhenwhy/Main.kt
@@ -1,5 +1,18 @@
 package kotlinwhyhowwhenwhy
 
-fun main() {
+import javafx.scene.paint.Color
+import kotlinwhyhowwhenwhy.classes.CustomerJava
+import kotlinwhyhowwhenwhy.expressions.ExpressionsJava
+import kotlinwhyhowwhenwhy.overloading.PrinterJava
 
+fun main() {
+    val customer = CustomerJava(0)
+    customer.name = "Fritz Wellblech"
+
+    val customer2: CustomerJava? = null
+    println(customer2?.name)
+
+    println(ExpressionsJava().isDrive(Color.RED))
+
+    PrinterJava.INSTANCE.printMessage("Hey Kotlin")
 }
